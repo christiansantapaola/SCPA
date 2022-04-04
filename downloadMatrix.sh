2d6a399eb2ef47f774bb57a3ab46a0974341634b
Christian Santapaola <9669chris@gmail.com>
2022-04-04 23:11:59 +0200

    - add script to download matrix to test.

---
+++ downloadMatrix.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/vanHeukelum/cage4.tar.gz
+
+wget https://sparse.tamu.edu/Bai/mhda416
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/HB/mcfe.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Bai/olm1000.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Sandia/adder_dcop_32.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/HB/west2021.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/DRIVCAV/cavity10.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Zitney/rdist2.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Williams/cant.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Simon/olafu.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Janna/Cube_Coup_dt0.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Janna/ML_Laplace.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/HB/bcsstk17.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Williams/mac_econ_fwd500.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Bai/mhd4800a.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Williams/cop20k_A.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Simon/raefsky2.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Bai/af23560.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Norris/lung2.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Fluorem/PR02R.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Botonakis/FEM_3D_thermal1.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Schmid/thermal1.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Schmid/thermal2.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Botonakis/thermomech_TK.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Schenk/nlpkkt80.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Williams/webbase-1M.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/IBM_EDA/dc1.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/SNAP/amazon0302.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Schenk_AFE/af_1_k101.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/SNAP/roadNet-PA.tar.gz
+
