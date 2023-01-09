7d50fa34461dedcacb6a4c2b626ee91fcace84b4
Christian Santapaola <9669chris@gmail.com>
2023-01-09 13:36:53 -0500

    add omp parallel section on ELL hybrid SpMV

--- src/spmv/SpMV.c
+++ src/spmv/SpMV.c
@@ -1,6 +1,6 @@
 #include "SpMV.h"
 
-int COOMatrix_SpMV(const COOMatrix *matrix, const Vector *x, Vector *y, Benchmark *benchmark) {
+int COOMatrix_SpMV(const COOMatrix* matrix, const Vector* x, Vector* y, Benchmark* benchmark) {
     clock_t start, end;
     if (!matrix || !x || !y) {
         return SPMV_FAIL;
@@ -16,12 +16,12 @@ int COOMatrix_SpMV(const COOMatrix *matrix, const Vector *x, Vector *y, Benchmar
     end = clock();
     if (benchmark) {
         benchmark->gpuTime = 0.0;
-        benchmark->cpuTime = ((double) (end - start)) / (double) CLOCKS_PER_SEC;
+        benchmark->cpuTime = ((double)(end - start)) / (double)CLOCKS_PER_SEC;
     }
     return SPMV_SUCCESS;
 }
 
-int CSRMatrix_SpMV(const COOMatrix *matrix, const Vector *x, Vector *y, u_int64_t N, Benchmark *benchmark) {
+int CSRMatrix_SpMV(const COOMatrix* matrix, const Vector* x, Vector* y, u_int64_t N, Benchmark* benchmark) {
     if (!matrix || !x || !y) {
         return SPMV_FAIL;
     }
@@ -29,23 +29,23 @@ int CSRMatrix_SpMV(const COOMatrix *matrix, const Vector *x, Vector *y, u_int64_
         benchmark->cpuTime = 0.0;
         benchmark->gpuTime = 0.0;
     }
-    CSRMatrix *csrMatrix = CSRMatrix_new_wpm(matrix);
+    CSRMatrix* csrMatrix = CSRMatrix_new_wpm(matrix);
     int cudaDev = CudaUtils_getBestDevice(csrMatrix->num_non_zero_elements * sizeof(float) + (x->size + y->size) * sizeof(float));
     CudaUtils_setDevice(cudaDev);
-    CSRMatrix *d_csrMatrix = CSRMatrix_to_CUDA(csrMatrix);
+    CSRMatrix* d_csrMatrix = CSRMatrix_to_CUDA(csrMatrix);
     for (u_int64_t i = 0; i < N; i++) {
-            float time;
-            CSRMatrix_SpMV_CUDA(cudaDev, d_csrMatrix, x, y, &time);
-            if (benchmark) {
-                benchmark->gpuTime += (double) time;
-            }
+        float time;
+        CSRMatrix_SpMV_CUDA(cudaDev, d_csrMatrix, x, y, &time);
+        if (benchmark) {
+            benchmark->gpuTime += (double)time;
+        }
     }
     CSRMatrix_free_CUDA(d_csrMatrix);
     CSRMatrix_free_wpm(csrMatrix);
     return SPMV_SUCCESS;
 }
 
-int ELLMatrix_SpMV(const COOMatrix *cooMatrix, const Vector *x, Vector *y, u_int64_t N, Benchmark *benchmark) {
+int ELLMatrix_SpMV(const COOMatrix* cooMatrix, const Vector* x, Vector* y, u_int64_t N, Benchmark* benchmark) {
     if (!cooMatrix || !x || !y) {
         return SPMV_FAIL;
     }
@@ -53,35 +53,36 @@ int ELLMatrix_SpMV(const COOMatrix *cooMatrix, const Vector *x, Vector *y, u_int
         benchmark->cpuTime = 0.0;
         benchmark->gpuTime = 0.0;
     }
-    COOMatrix *high, *low;
+    COOMatrix* high, * low;
     high = COOMatrix_new();
     low = COOMatrix_new();
     int notSplit = COOMatrix_split(cooMatrix, low, high, 64);
     if (notSplit) {
-        ELLMatrix *ellMatrix = ELLMatrix_new_fromCOO_wpm(cooMatrix);
-        ELLMatrix *d_ellMatrix = ELLMatrix_to_CUDA(ellMatrix);
+        ELLMatrix* ellMatrix = ELLMatrix_new_fromCOO_wpm(cooMatrix);
+        ELLMatrix* d_ellMatrix = ELLMatrix_to_CUDA(ellMatrix);
         int cudaDev = CudaUtils_getBestDevice(ellMatrix->data_size * sizeof(float) + (x->size + y->size) * sizeof(float));
         CudaUtils_setDevice(cudaDev);
         for (u_int64_t i = 0; i < N; i++) {
-                float time;
-                ELLMatrix_SpMV_CUDA(cudaDev, d_ellMatrix, x, y, &time);
-                if (benchmark) {
-                    benchmark->gpuTime += (double) time;
-                }
+            float time;
+            ELLMatrix_SpMV_CUDA(cudaDev, d_ellMatrix, x, y, &time);
+            if (benchmark) {
+                benchmark->gpuTime += (double)time;
+            }
         }
         ELLMatrix_free_CUDA(d_ellMatrix);
         ELLMatrix_free_wpm(ellMatrix);
-    } else {
-        ELLMatrix *ellMatrix = ELLMatrix_new_fromCOO_wpm(low);
-        ELLMatrix *d_ellMatrix = ELLMatrix_to_CUDA(ellMatrix);
+    }
+    else {
+        ELLMatrix* ellMatrix = ELLMatrix_new_fromCOO_wpm(low);
+        ELLMatrix* d_ellMatrix = ELLMatrix_to_CUDA(ellMatrix);
         int cudaDev = CudaUtils_getBestDevice(ellMatrix->data_size * sizeof(float) + (x->size + y->size) * sizeof(float));
         CudaUtils_setDevice(cudaDev);
         for (u_int64_t i = 0; i < N; i++) {
-                float time;
-                ELLCOOMatrix_SpMV_CUDA(cudaDev, d_ellMatrix, high, x, y, &time);
-                if (benchmark) {
-                    benchmark->gpuTime += (double) time;
-                }
+            float time;
+            ELLCOOMatrix_SpMV_CUDA(cudaDev, d_ellMatrix, high, x, y, &time);
+            if (benchmark) {
+                benchmark->gpuTime += (double)time;
+            }
         }
         ELLMatrix_free_CUDA(d_ellMatrix);
         ELLMatrix_free_wpm(ellMatrix);
@@ -91,7 +92,7 @@ int ELLMatrix_SpMV(const COOMatrix *cooMatrix, const Vector *x, Vector *y, u_int
     return SPMV_SUCCESS;
 }
 
-int CSRMatrix_SpMV_cpu(const COOMatrix *matrix, const Vector *x, Vector *y, u_int64_t N, Benchmark *benchmark) {
+int CSRMatrix_SpMV_cpu(const COOMatrix* matrix, const Vector* x, Vector* y, u_int64_t N, Benchmark* benchmark) {
     if (!matrix || !x || !y) {
         return SPMV_FAIL;
     }
@@ -99,51 +100,62 @@ int CSRMatrix_SpMV_cpu(const COOMatrix *matrix, const Vector *x, Vector *y, u_in
         benchmark->cpuTime = 0.0;
         benchmark->gpuTime = 0.0;
     }
-    CSRMatrix *csrMatrix = CSRMatrix_new(matrix);
+    CSRMatrix* csrMatrix = CSRMatrix_new(matrix);
     Benchmark tmp;
     for (u_int64_t i = 0; i < N; i++) {
-            CSRMatrix_SpMV_OMP(csrMatrix, x, y, &tmp);
-            if (benchmark) {
-                benchmark->gpuTime += tmp.gpuTime;
-                benchmark->cpuTime += tmp.cpuTime;
-            }
+        CSRMatrix_SpMV_OMP(csrMatrix, x, y, &tmp);
+        if (benchmark) {
+            benchmark->gpuTime += tmp.gpuTime;
+            benchmark->cpuTime += tmp.cpuTime;
+        }
     }
     CSRMatrix_free(csrMatrix);
     return SPMV_SUCCESS;
 }
 
-int ELLMatrix_SpMV_cpu(const COOMatrix *matrix, const Vector *x, Vector *y, u_int64_t N, Benchmark *benchmark) {
-        if (!matrix || !x || !y) {
+int ELLMatrix_SpMV_cpu(const COOMatrix* matrix, const Vector* x, Vector* y, u_int64_t N, Benchmark* benchmark) {
+    if (!matrix || !x || !y) {
         return SPMV_FAIL;
     }
     if (benchmark) {
         benchmark->cpuTime = 0.0;
         benchmark->gpuTime = 0.0;
     }
-    COOMatrix *first = COOMatrix_new();
-    COOMatrix *second = COOMatrix_new();
+    COOMatrix* first = COOMatrix_new();
+    COOMatrix* second = COOMatrix_new();
     int notSplit = COOMatrix_split(matrix, first, second, 64);
     if (notSplit) {
-    ELLMatrix *ellMatrix = ELLMatrix_new_fromCOO(matrix);
-    Benchmark tmp;
-    for (u_int64_t i = 0; i < N; i++) {
+        ELLMatrix* ellMatrix = ELLMatrix_new_fromCOO(matrix);
+        Benchmark tmp;
+        for (u_int64_t i = 0; i < N; i++) {
             ELLMatrix_SpMV_OMP(ellMatrix, x, y, &tmp);
             if (benchmark) {
                 benchmark->gpuTime += tmp.gpuTime;
                 benchmark->cpuTime += tmp.cpuTime;
             }
+        }
+        ELLMatrix_free(ellMatrix);
     }
-    ELLMatrix_free(ellMatrix);
-    } else {
-        ELLMatrix *ellMatrix = ELLMatrix_new_fromCOO(first);
+    else {
+        ELLMatrix* ellMatrix = ELLMatrix_new_fromCOO(first);
         Benchmark ellTmp, cooTmp;
         for (u_int64_t i = 0; i < N; i++) {
-                ELLMatrix_SpMV_OMP(ellMatrix, x, y, &ellTmp);
-                COOMatrix_SpMV(second, x, y, &cooTmp);
-                if (benchmark) {
-                    benchmark->gpuTime += cooTmp.gpuTime + ellTmp.gpuTime;
-                    benchmark->cpuTime += cooTmp.cpuTime + ellTmp.cpuTime;
+#pragma omp parallel sections
+            {
+#pragma omp section
+                {
+                    ELLMatrix_SpMV_OMP(ellMatrix, x, y, &ellTmp);
+
                 }
+#pragma omp section
+                {
+                    COOMatrix_SpMV(second, x, y, &cooTmp);
+                }
+            }
+            if (benchmark) {
+                benchmark->gpuTime += cooTmp.gpuTime + ellTmp.gpuTime;
+                benchmark->cpuTime += cooTmp.cpuTime + ellTmp.cpuTime;
+            }
         }
         ELLMatrix_free(ellMatrix);
     }
